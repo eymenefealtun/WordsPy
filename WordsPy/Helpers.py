38512ecfd79bb5b742bffe1cded5ada9d5b8f3ed
eymenefealtun <eymenefealtun@hotmail.com>
2024-02-02 00:58:13 +0300

    feat: some stuff added

---
+++ WordsPy/Helpers.py
@@ -0,0 +1,13 @@
+import requests
+
+
+def get_main_url(language):
+    return f"https://raw.githubusercontent.com/eymenefealtun/all-words-in-all-languages/main/{language}/{language}.txt"
+
+
+def extract_array(language_name):
+    return requests.get(get_main_url(language_name)).text.split(",")
+
+
+async def extract_array_async(language_name):
+    print("async is currently not available. use default one!")
